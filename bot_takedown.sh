212ab67866e0273409884a3f50b689ea5368f59a
Krishna Sai Marella <34278146+Presidentsu@users.noreply.github.com>
2021-07-27 20:41:56 +0530

    bot_takedown challenge

---
+++ bot_takedown.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+ip = <iplist file> #make sure to remove <iplist file> with your fine name
+for i in `cat ip`
+do
+curl <url attack command> #Rename it with the url or the command that attacks a bot
+done
